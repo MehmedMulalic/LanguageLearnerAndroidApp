4c29904246bec7a60e9c2653e84e241976067d4f
Mehmed Mulalić <meh.mulalic@gmail.com>
2026-07-11 12:53:01 +0200

    Included an optional icon

--- app/src/main/java/com/mmulalic/languagelearner/ui/main/home/ProfileItem.kt
+++ app/src/main/java/com/mmulalic/languagelearner/ui/main/home/ProfileItem.kt
@@ -1,6 +1,9 @@
 package com.mmulalic.languagelearner.ui.main.home
 
+import androidx.compose.ui.graphics.vector.ImageVector
+
 data class ProfileItem(
     val label: String = "",
-    val onClick: () -> Unit
+    val onClick: () -> Unit,
+    val icon: ImageVector? = null
 )
