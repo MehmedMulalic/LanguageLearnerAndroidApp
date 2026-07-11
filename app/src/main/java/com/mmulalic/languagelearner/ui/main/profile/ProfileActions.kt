f9799b5b5bffb20ebfc52a64f7dfab2bb1fb2666
Mehmed Mulalić <meh.mulalic@gmail.com>
2026-07-11 14:37:39 +0200

    Edited ProfileScreen.kt

---
+++ app/src/main/java/com/mmulalic/languagelearner/ui/main/profile/ProfileActions.kt
@@ -0,0 +1,10 @@
+package com.mmulalic.languagelearner.ui.main.profile
+
+data class ProfileActions(
+    val onProfileClick: () -> Unit = {},
+    val onEmailClick: () -> Unit = {},
+    val onChangePasswordClick: () -> Unit = {},
+    val onNotificationsClick: () -> Unit = {},
+    val onDarkModeClick: () -> Unit = {},
+    val onLogoutClick: () -> Unit = {}
+)
--- app/src/main/java/com/mmulalic/languagelearner/ui/main/profile/ProfileScreen.kt
+++ app/src/main/java/com/mmulalic/languagelearner/ui/main/profile/ProfileScreen.kt
@@ -1,16 +1,30 @@
 package com.mmulalic.languagelearner.ui.main.profile
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.automirrored.outlined.Logout
+import androidx.compose.material.icons.outlined.DarkMode
+import androidx.compose.material.icons.outlined.Key
+import androidx.compose.material.icons.outlined.Mail
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -18,10 +32,11 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
-import com.mmulalic.languagelearner.ui.main.home.ProfileItem
 
 @Composable
 fun ProfileScreen(
@@ -30,24 +45,26 @@ fun ProfileScreen(
     profileViewModel: ProfileViewModel = hiltViewModel()
 ) {
     val state by profileViewModel.state.collectAsState()
-    val options = listOf(
-        ProfileItem("Settings") {}, //TODO
-        ProfileItem("Logout") {
-            profileViewModel.logout()
-        }
-    )
 
     LaunchedEffect(state) {
         if (state is ProfileState.LoggedOut) {
-            onSignoutSucces()
+            onSignOutSuccess()
         }
     }
 
     when (state) {
-        ProfileState.Loading -> LoadingForm()
-        ProfileState.LoggedIn -> ProfileForm(modifier, options)
-        ProfileState.LoggedOut -> LoadingForm()
-        is ProfileState.Error -> ProfileForm(modifier, options)
+        ProfileState.Loading, ProfileState.LoggedOut -> LoadingForm()
+        ProfileState.LoggedIn, is ProfileState.Error -> ProfileForm(
+            modifier = modifier,
+            actions = ProfileActions(
+                onProfileClick = {},
+                onEmailClick = {},
+                onChangePasswordClick = {},
+                onNotificationsClick = {},
+                onDarkModeClick = {},
+                onLogoutClick = profileViewModel::logout
+            )
+        )
     }
 }
 
@@ -64,29 +81,130 @@ fun LoadingForm() {
 @Composable
 fun ProfileForm(
     modifier: Modifier = Modifier,
-    options: List<ProfileItem>
+    actions: ProfileActions = ProfileActions()
 ) {
-    Column {
-        options.forEach { (label, onClick) ->
-            Row(
-                modifier = modifier
-                    .fillMaxWidth()
-                    .clickable { onClick() }
-                    .padding(16.dp),
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(
-                    label,
-                    style = MaterialTheme.typography.bodyLarge
-                )
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Column {
+                SettingsHeader("Account")
+                SettingsCard {
+                    SettingsRow(
+                        "Profile",
+                        actions.onProfileClick,
+                        Icons.Outlined.Person
+                    )
+                    SettingsRow(
+                        "Email Address",
+                        actions.onEmailClick,
+                        Icons.Outlined.Mail
+                    )
+                    SettingsRow(
+                        "Change Password",
+                        actions.onChangePasswordClick,
+                        Icons.Outlined.Key
+                    )
+                }
             }
-            HorizontalDivider()
+
+            Column {
+                SettingsHeader("Personalization")
+                SettingsCard {
+                    SettingsRow(
+                        "Notifications",
+                        actions.onNotificationsClick,
+                        Icons.Outlined.Notifications
+                    )
+                    SettingsRow(
+                        "Dark Mode",
+                        actions.onDarkModeClick,
+                        Icons.Outlined.DarkMode
+                    )
+                }
+            }
+
+            Column {
+                SettingsCard {
+                    SettingsRow(
+                        "Log Out",
+                        actions.onLogoutClick,
+                        Icons.AutoMirrored.Outlined.Logout,
+                        MaterialTheme.colorScheme.error
+                    )
+                }
+            }
         }
+    }
+}
+
+@Composable
+fun SettingsHeader(
+    label: String
+) {
+    Text(
+        label,
+        style = MaterialTheme.typography.titleMedium,
+        color = MaterialTheme.colorScheme.secondary,
+        modifier = Modifier.padding(horizontal = 24.dp)
+    )
+}
+
+@Composable
+fun SettingsCard(
+    content: @Composable ColumnScope.() -> Unit
+) {
+    Card(
+        shape = RoundedCornerShape(12.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column(content = content)
+    }
+}
+
+@Composable
+fun SettingsRow(
+    label: String,
+    onClick: () -> Unit,
+    icon: ImageVector,
+    color: Color? = null
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            icon,
+            contentDescription = null,
+            tint = color ?: LocalContentColor.current
+        )
+        Text(
+            text = label,
+            color = color ?: LocalContentColor.current,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier
+                .weight(1f)
+                .padding(horizontal = 8.dp, vertical = 4.dp)
+        )
+        Icon(
+            Icons.AutoMirrored.Filled.KeyboardArrowRight,
+            contentDescription = null,
+            tint = color ?: LocalContentColor.current
+        )
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun ProfileFormPreview() {
-    ProfileForm(Modifier,listOf(ProfileItem("Test") {}))
+    ProfileForm(Modifier)
 }
