f2cda9bb1da690bdfca8cf5553d71f4e2e45a7a1
Mehmed Mulalić <meh.mulalic@gmail.com>
2026-07-14 16:34:07 +0200

    Replaced theme with Material Builder's theme
    Included edge-to-edge with automatic system tray coloring

--- app/src/main/java/com/mmulalic/languagelearner/MainActivity.kt
+++ app/src/main/java/com/mmulalic/languagelearner/MainActivity.kt
@@ -3,23 +3,26 @@ package com.mmulalic.languagelearner
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.mmulalic.languagelearner.ui.auth.AuthenticationScreen
 import com.mmulalic.languagelearner.ui.main.profile.personalization.ThemeViewModel
-import com.mmulalic.languagelearner.ui.theme.LanguageLearnerTheme
+import com.mmulalic.languagelearner.ui.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
         setContent {
             val themeViewModel: ThemeViewModel = hiltViewModel()
             val themeOption by themeViewModel.themeOption.collectAsState()
 
-            LanguageLearnerTheme(themeOption = themeOption) {
+            AppTheme(themeOption = themeOption) {
                 AuthenticationScreen()
             }
         }
---
+++ app/src/main/java/com/mmulalic/languagelearner/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.mmulalic.languagelearner.ui.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
